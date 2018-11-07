5594279ef9baa9d5ba8ff841af6b5d9c7ac37af3
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-07 18:11:55 +0000

    Test 5: Comprobacion de variables[Acierto]

--- lib/nutrition.rb
+++ lib/nutrition.rb
@@ -2,8 +2,8 @@ class Nutrition
 	
 	attr_reader :saturadas, :monoinsaturadas, :polinsaturadas, :azucares, :polialcoholes, :almidon, :fibra, :proteinas, :sal
 
-	def initialize(saturadas, monoinsaturados, polinsaturadas, azucares, polialcoholes, almidon, fibra, proteinas, sal)
-		@saturadas, @monoinsaturados, @polinsaturadas, @azucares, @polialcoholes, @almidon, @fibra, @proteinas, @sal = saturadas, monoinsaturados, polinsaturadas, azucares, polialcoholes, almidon, fibra, proteinas, sal
+	def initialize(saturadas, monoinsaturadas, polinsaturadas, azucares, polialcoholes, almidon, fibra, proteinas, sal)
+		@saturadas, @monoinsaturadas, @polinsaturadas, @azucares, @polialcoholes, @almidon, @fibra, @proteinas, @sal = saturadas, monoinsaturadas, polinsaturadas, azucares, polialcoholes, almidon, fibra, proteinas, sal
 	end
 
 end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -6,5 +6,45 @@ RSpec.describe Nutrition do
 		@nutrition = Nutrition.new(10,10,10,10,10,10,10,10,10)
 	end
 
+	describe "Etiqueta nutricional" do
+		
+		it "Almacenado correctamente: Grasas saturadas" do
+			expect(@nutrition.saturadas).to eq(10)
+		end
+
+		it "Almacenado correctamente: Grasas monoinsaturadas" do
+			expect(@nutrition.monoinsaturadas).to eq(10)
+		end
+
+		it "Almacenada correctamente: Grasas polinsaturadas" do
+			expect(@nutrition.polinsaturadas).to eq(10)
+		end
+
+		it "Almacenado correctamente: Azucares" do
+			expect(@nutrition.azucares).to eq(10)
+		end
+
+		it "Almacenado correctamente: Polialcoholes" do
+			expect(@nutrition.polialcoholes).to eq(10)
+		end
+
+		it "Almacenado correctamente: Almidon" do
+			expect(@nutrition.almidon).to eq(10)
+		end
+
+		it "Almacenado correctamente: Fibra" do
+			expect(@nutrition.fibra).to eq (10)
+		end
+
+		it "Almacenado correctamente: Proteinas" do
+			expect(@nutrition.proteinas).to eq(10)
+		end
+
+		it "Almacenado correctamente: Sal" do
+			expect(@nutrition.sal).to eq(10)
+		end
+
+	end
+
 
 end
