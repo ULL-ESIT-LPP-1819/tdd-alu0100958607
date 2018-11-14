34755e63d4ad88f77ce4a96beb71e85833ad1ab1
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-14 11:08:39 +0000

    commit 5: Declaración de la lista[Fallo]

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,9 +1,10 @@
 require 'prct06/nutrition'
 require 'prct06/list.rb'
 
-RSpec.describe Nutrition do
+RSpec.describe List do
 	before :each do
 		@node1 = Node.new(1,NIL,NIL)
 		@node2 = Node.new(2,NIL,NIL)
+		@list = List.new()
 	end
 end
