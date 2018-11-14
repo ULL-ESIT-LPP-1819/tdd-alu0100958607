753311a05b45a10f4e0b1c5e57d071b4107de1f5
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-14 10:51:52 +0000

    commit 4: Declaración de node[Acierto]

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,7 +1,7 @@
 require 'prct06/nutrition'
-require 'prct06/list'
+require 'prct06/list.rb'
 
-RSpec.describe List do
+RSpec.describe Nutrition do
 	before :each do
 		@node1 = Node.new(1,NIL,NIL)
 		@node2 = Node.new(2,NIL,NIL)
