a395cea7b2400346189197274f08e2948de30062
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-20 15:17:53 +0000

    Comprobación de la pertenencia de @list a la clase List[Acierto]

--- spec/antro_spec.rb
+++ spec/antro_spec.rb
@@ -1,8 +1,16 @@
 require 'prct06/nutrition'
 require 'prct06/list.rb'
-require 'prct06/antro.rb'
+#require 'prct06/antro.rb'
 
-RSpec.describe Antro do
+RSpec.describe Prct06 do
+	before :each do
+                @list = List.new(nil,nil)
+        end
 
+        describe "Tests de herencia y clase antropometrica" do
+                it "Existe la clase List" do
+                        expect(@list.class).to eq(List)
+                end
+        end
 end
 
