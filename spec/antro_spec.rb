076c364e678db8e823638825d401c6e539743721
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-20 15:22:29 +0000

    Comprobación que @list es instancia de la clase List[Acierto]

--- spec/antro_spec.rb
+++ spec/antro_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe Prct06 do
                 it "Existe la clase List" do
                         expect(@list.class).to eq(List)
                 end
+
+		it "Comprobando que @list es instancia de la clase List" do
+			expect(@list.instance_of? List).to eq(true)
+		end
         end
 end
 
