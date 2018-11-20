e3e5821128712efb022878561ba1a25289cc1475
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-11-20 15:28:01 +0000

    Comprobación que @nutrition es instancia de la clase Nutrition[Acierto]

--- spec/antro_spec.rb
+++ spec/antro_spec.rb
@@ -5,6 +5,7 @@ require 'prct06/list.rb'
 RSpec.describe Prct06 do
 	before :each do
                 @list = List.new(nil,nil)
+		@nutrition = Nutrition.new(1,1,1,1,1,1,1,1,1,"Oreo")
         end
 
         describe "Tests de herencia y clase antropometrica" do
@@ -15,6 +16,19 @@ RSpec.describe Prct06 do
 		it "Comprobando que @list es instancia de la clase List" do
 			expect(@list.instance_of? List).to eq(true)
 		end
+
+		it "Comprobando que @list es un objeto de la clase List" do
+			expect(@list.is_a? List).to eq(true)
+		end
+
+		it "Comprobando que @nutrition pertenece a la clase Nutrition" do
+			expect(@nutrition.class).to eq(Nutrition)
+		end
+
+		it "Comprobando que @nutrition es instancia de la clase Nutrition" do
+			expect(@nutrition.instance_of? Nutrition).to eq(true)
+		end
+
         end
 end
 
