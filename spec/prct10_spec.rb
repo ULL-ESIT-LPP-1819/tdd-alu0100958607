95f5d2e2c265ce0c69d21be4830cccc7ecfe8aba
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-12-11 18:15:07 +0000

    Practica 10

---
+++ spec/prct10_spec.rb
@@ -0,0 +1,24 @@
+
+RSpec.describe prct10 do
+	before :each do
+		
+		@a1 = Antro.new(90,190,3,4,5,6,7)
+		@a2 = Antro.new(80,195,3,4,5,6,7)
+		@a3 = Antro.new(70,170,3,4,5,6,7)
+		@a4 = Antro.new(65,150,3,4,5,6,7)
+		@a5 = Antro.new(100,180,3,4,5,6,7)
+
+		@e1 = Nutrition.new(10,10,10,10,10,10,10,10,10,"Ensaladilla rusa")
+	        @e2 = Nutrition.new(10,10,10,10,10,10,10,10,10,"Sandwich mixto")
+		@e3 = Nutrition.new(10,10,10,10,10,10,10,10,10,"Potaje de lentejas")
+		@e4 = Nutrition.new(10,10,10,10,10,10,10,10,10,"Bistec de cerdo")
+		@e5 = Nutrition.new(10,10,10,10,10,10,10,10,10,"Ensalada de atun")
+
+
+
+	end
+
+	describe "Pruebas menu" do
+
+	end	
+end
