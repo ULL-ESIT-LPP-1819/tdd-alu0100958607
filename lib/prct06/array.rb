1411ace1995cf7c00d3b8a14a9ffe05412ddae3b
Sergio Rodriguez <alu0100958607@ull.edu.es>
2018-12-18 16:34:02 +0000

    Comprobando el metodo sort_for de array [Acierto]

--- lib/prct06/array.rb
+++ lib/prct06/array.rb
@@ -1,6 +1,19 @@
 class Array
 	
 	def sort_for
+
+		@aux = self
+
+		for x in 0..self.length-1
+			for y in 0..length-2-x
+				if ( @aux[y] > @aux[y+1] )
+					@aux[y], @aux[y+1] = @aux[y+1], @aux[y] 
+				end
+			end
+		end
+
+		return @aux
+
 	end
 
 	def sort_each
